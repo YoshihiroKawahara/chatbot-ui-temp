34f4f099ddadd2b359b9abf104063eb09ae92581
Yoshihiro Kawahara <35133806+YoshihiroKawahara@users.noreply.github.com>
2025-03-22 17:38:06 +0900

    Update llms.ts

--- types/llms.ts
+++ types/llms.ts
@@ -11,10 +11,13 @@ export type LLMID =
 // OpenAI Models (UPDATED 5/13/24)
 export type OpenAILLMID =
   | "gpt-4o" // GPT-4o
-  | "gpt-4-turbo-preview" // GPT-4 Turbo
-  | "gpt-4-vision-preview" // GPT-4 Vision
-  | "gpt-4" // GPT-4
-  | "gpt-3.5-turbo" // Updated GPT-3.5 Turbo
+  | "gpt-4o-mini" // GPT-4o-mini
+  | "o3-mini" // o3-mini
+  | "o1" // o1
+  // | "gpt-4-turbo-preview" // GPT-4 Turbo
+  // | "gpt-4-vision-preview" // GPT-4 Vision
+  // | "gpt-4" // GPT-4
+  // | "gpt-3.5-turbo" // Updated GPT-3.5 Turbo
 
 // Google Models
 export type GoogleLLMID =
